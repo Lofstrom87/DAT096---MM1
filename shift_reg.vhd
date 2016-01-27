09f1d91f49a55555ad16ac71fb9fdd6fccc88c5e
HenrikChalmers <johanshe@student.chalmers.se>
2016-01-27 16:40:21 +0100

    Aesthetic and comment code
    Using nasty CAPITAL LETTERS

--- shift_reg.vhd
+++ shift_reg.vhd
@@ -1,22 +1,26 @@
 -- Generic shift register.
--- Used in I2S
+-- USEd in I2S
 -- 2016-01-27
+-- Samples signals and shifts register values every clock pulse (serial clock).
+-- When word_select changes value the value in the shift registry is sent to the parallel_out
 
-library ieee;
-use ieee.STD_LOGIC_1164.all;
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
 
-entity SHIFT_REG is
-	generic(N: integer := 12);
+ENTITY shift_reg IS
+	generic(N: integer := 12); -- Number of registers used.
 	port(
-		serial_in, clk, reset, word_select: in STD_LOGIC;
-		parallel_out: out STD_LOGIC_VECTOR (N-1 downto 0)
+		serial_in, clk, reset, word_select: IN STD_LOGIC;
+		parallel_out: OUT STD_LOGIC_VECTOR (N-1 downto 0)
 	);
-end SHIFT_REG;
+END shift_reg;
 
-architecture SHIFT_REG_architecture of  SHIFT_REG is
-	CONSTANT RESET_ACTIVE: STD_LOGIC := '1'; --Choose if reset is active high or low.
+ARCHITECTURE shift_reg_architecture OF shift_reg IS
+	-- Constants
+	CONSTANT RESET_ACTIVE: STD_LOGIC := '1'; -- Choose if reset is active high or low.
 
-	SIGNAL s_interconnect_register: STD_LOGIC_VECTOR(N downto 0);
+	-- Signals
+	SIGNAL s_interconnect_register: STD_LOGIC_VECTOR(N downto 0); -- Signal that interconnects the registers.
 COMPONENT FF_REG IS
    PORT(
       clk 	 : IN STD_LOGIC;
@@ -24,9 +28,9 @@ COMPONENT FF_REG IS
       d 	 : IN STD_LOGIC;
       q 	 : OUT STD_LOGIC
    );
-end COMPONENT;
+END COMPONENT;
 
-begin
+BEGIN
 
 registers: for i in 0 to N-1 generate
 		mux_i_bit: FF_REG port map(
@@ -35,15 +39,15 @@ registers: for i in 0 to N-1 generate
 			d => s_interconnect_register(i),
 			q => s_interconnect_register(i+1)
 		);
-	end generate;
+	END generate;
 
-process(word_select, reset)
-begin
+PROCESS(word_select, reset)
+BEGIN
 	if(reset = RESET_ACTIVE) then
-		parallel_out <= (others => '0');
+		parallel_out <= (others => '0'); -- Reset parallel output.
 	elsif(word_select'event) then
-		parallel_out <= s_interconnect_register(N downto 1);
-	end if;
-end process;
+		parallel_out <= s_interconnect_register(N downto 1); -- Output the values from the shift registers to the parallel output.
+	END if;
+END PROCESS;
 
-end SHIFT_REG_architecture;
+END shift_reg_architecture;
